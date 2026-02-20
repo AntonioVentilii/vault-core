02684cabeba1b015fd7965b2ba24deac5d6bfd90
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-02-21 00:47:30 +0100

    ci: Release flow

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,8 @@
 
 cargo build --locked --target wasm32-unknown-unknown --release -p bucket
 
+gzip -c target/wasm32-unknown-unknown/release/bucket.wasm >target/wasm32-unknown-unknown/release/bucket.wasm.gz
+
 cargo build --locked --target wasm32-unknown-unknown --release -p directory
+
+gzip -c target/wasm32-unknown-unknown/release/directory.wasm >target/wasm32-unknown-unknown/release/directory.wasm.gz
